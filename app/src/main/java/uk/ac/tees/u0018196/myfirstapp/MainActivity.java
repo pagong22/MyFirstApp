87ad56ecb4ac9427081407d52e224a43b88e9b96
Tyrone Davison <t.p.davison@tees.ac.uk>
2020-01-27 13:37:37 +0000

    Update to latest SDK and tools

--- app/src/main/java/uk/ac/tees/u0018196/myfirstapp/MainActivity.java
+++ app/src/main/java/uk/ac/tees/u0018196/myfirstapp/MainActivity.java
@@ -1,6 +1,6 @@
 package uk.ac.tees.u0018196.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
